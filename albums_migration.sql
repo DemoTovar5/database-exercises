1b2d897e0606c82f80c88bfddaa83eccfcb03cf1
DemoTovar5 <demetrio.tovar5@gmail.com>
2021-07-26 14:34:20 -0500

    tables

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,2 +1,5 @@
 USE codeup_test_db;
 
+CREATE TABLE albums(id INT UNSIGNED NOT NULL AUTO_INCREMENT , artist VARCHAR(50), name VARCHAR(50), release_date INT,
+ sales FLOAT, genre VARCHAR(50), PRIMARY KEY(id));
+
